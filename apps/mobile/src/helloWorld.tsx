1481dc6f719809158cec7525747d5a727d9696d6
Thomas5109 <Thomastm5109@gmail.com>
2026-09-15 17:55:45 -0400

    fix: adiciona arquivod do frontend mobile ao repositorio e criação do primeiro servidor fastify

---
+++ apps/mobile/src/helloWorld.tsx
@@ -0,0 +1,16 @@
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
+
+export default YourApp;
